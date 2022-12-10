5bb12d5dda40918bae212452670b4626b77f2561
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2022-12-10 20:42:09 -0300

    Finished home screen design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List _itens = [
+    {
+      "icon": Icons.compost,
+      "title": "Conheça a SEMAS",
+    },
+    {
+      "icon": Icons.sunny,
+      "title": "Previsão do Tempo",
+    },
+    {
+      "icon": Icons.connect_without_contact,
+      "title": "Redes Sociais",
+    },
+    {
+      "icon": Icons.library_books,
+      "title": "Consultar Processos SIMLAM",
+    },
+    {
+      "icon": Icons.manage_search,
+      "title": "Consultar Portal da Transparência",
+    },
+    {
+      "icon": Icons.language,
+      "title": "Acessar Links Úteis",
+    },
+    {
+      "icon": Icons.announcement,
+      "title": "Realizar Denúncias na Ouvidoria",
+    },
+    {
+      "icon": Icons.support_agent,
+      "title": "Solicitar Chamados no CATIS",
+    },
+    {
+      "icon": Icons.quiz,
+      "title": "FAQs",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +62,69 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Expanded(
-            child: SingleChildScrollView(
-              child: Text("Hello World!"),
+          16.height,
+          const Text(
+            "SEMAS",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          16.height,
+          const Padding(
+            padding: EdgeInsets.only(left: 16.0),
+            child: Text(
+              "Clique no ícone que você deseja acessar",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 30,
+              ),
+              itemCount: _itens.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: Icon(
+                        _itens[index]["icon"],
+                        size: 40,
+                        color: const Color(0xff0056a3),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      _itens[index]["title"],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
           Container(
