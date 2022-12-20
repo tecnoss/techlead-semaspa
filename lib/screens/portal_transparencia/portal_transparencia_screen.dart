2e37c5afc447c105ea9df7d69f3a61c47dc28fd7
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2022-12-20 16:46:13 -0300

    Finished portal da transparência screen

--- lib/screens/portal_transparencia/portal_transparencia_screen.dart
+++ lib/screens/portal_transparencia/portal_transparencia_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PortalTransparenciaScreen extends StatefulWidget {
   const PortalTransparenciaScreen({super.key});
@@ -9,14 +10,47 @@ class PortalTransparenciaScreen extends StatefulWidget {
 }
 
 class _PortalTransparenciaScreenState extends State<PortalTransparenciaScreen> {
+  final WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(NavigationDelegate(
+      onProgress: (int progress) {},
+      onPageStarted: (url) {},
+      onPageFinished: (url) {},
+      onWebResourceError: (error) {},
+      onNavigationRequest: (NavigationRequest request) {
+        return NavigationDecision.navigate;
+      },
+    ))
+    ..loadRequest(Uri.parse(
+        'http://portaldatransparencia.semas.pa.gov.br/#/visao-publica'));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Portal da Transparência'),
-      ),
-      body: const Center(
-        child: Text('Portal da Transparência'),
+      backgroundColor: Colors.grey.shade100,
+      extendBodyBehindAppBar: true,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebViewWidget(
+              controller: controller,
+            ),
+            Positioned(
+              top: 8,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
