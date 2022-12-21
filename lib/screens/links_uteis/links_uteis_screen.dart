3588f544436add367c1dc80fb77d09dfc8cc13b6
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2022-12-20 21:28:13 -0300

    Finished links uteis

--- lib/screens/links_uteis/links_uteis_screen.dart
+++ lib/screens/links_uteis/links_uteis_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:semasma/widgets/bottom_bar.dart';
+import 'package:semasma/widgets/leading.dart';
+import 'package:semasma/widgets/screen_title.dart';
+import 'package:semasma/widgets/title_app_bar.dart';
 
 class LinksUteisScreen extends StatefulWidget {
   const LinksUteisScreen({super.key});
@@ -12,10 +17,97 @@ class _LinksUteisScreenState extends State<LinksUteisScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Links Úteis'),
+        title: const TitleAppBar(),
+        leading: const Leading(),
       ),
-      body: const Center(
-        child: Text('Links Úteis'),
+      bottomNavigationBar: const BottomBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const ScreenTitle(title: 'Acessar Links Úteis'),
+              const Text(
+                'Escolha o sistema que deseja acessar:',
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              16.height,
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Image.asset(
+                          'assets/icons/links_uteis/brasao-semas.png',
+                          width: 100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Image.asset(
+                          'assets/icons/links_uteis/selo-verde.png',
+                          width: 100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Image.asset(
+                          'assets/icons/links_uteis/sislam.png',
+                          width: 100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Image.asset(
+                          'assets/icons/links_uteis/car.png',
+                          width: 100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
